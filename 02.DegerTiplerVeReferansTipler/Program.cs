1e8dbdd4af84ce2c8782da8062d2cf51e5f08b31
Elif Tuba Korkmaz <etubasavli@gmail.com>
2023-07-15 11:35:22 +0300

    .

---
+++ 02.DegerTiplerVeReferansTipler/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hello, World!");
+
+string name = "Elif";
+string name2 = name;
+name2 = "Tuba";
+
+Console.WriteLine(name);
+Console.WriteLine(name2); // deger tip örneği
+
+User user = new();
+User user2 = user;
+user2.Name = "Tuba";
+
+Console.WriteLine(user.Name);
+
+class User
+{
+    public string Name = "Elif";
+}
---
+++ 03.Operatörler/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// Operatörler
+// == kontrol operatörü
+
+int a = 0;
+int b = 1;
+
+var c = (a == b);
+
+// + - * / matematiksel operatörler
+
+int r = ((5 * 2) + 5) - 2;
+
+// += && -=
+
+int z = 1;
+
+z += 5; // z = z + 5;
+
+// ++;
+// --;
+
+int x = 1;
+x++;
+x--;
+
+// + operatörü string değerlerde birleştirme işlemi yapar;
+
+string name = "Elif" + " " + "Tuba";
+string name2 = $"{name} 26 yaşında";
+
+// @ operatörü \ kullanabilmemiz için kullanılır
+
+string url = @"http:\";
+
+var result = name + 2; // Taner Saydam2
+
+//% 
+
+var r2 = (x % 2);
+
