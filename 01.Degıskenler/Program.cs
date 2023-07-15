bbb4b673c84cddbcf8266bb1d14c6071fae31717
Elif Tuba Korkmaz <etubasavli@gmail.com>
2023-07-15 11:06:31 +0300

    .

--- 01.Degıskenler/Program.cs
+++ 01.Degıskenler/Program.cs
@@ -1,1 +1,19 @@
 ﻿
+Console.WriteLine("Elif Tuba Korkmaz");
+
+//string, int, decimal, double, float, bool, DateTime, Object
+
+string name; // ataması değeri null => boş 
+
+int age; // atamasız değeri 0
+
+bool isActive; // atamasız değeri false 
+
+string nameLastName = "Elif Tuba Korkmaz";
+
+name = "asdasd";
+
+char karakter = 'A';
+
+Console.WriteLine(nameLastName);
+
