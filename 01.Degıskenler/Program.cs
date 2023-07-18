1aeec10ecf39802c98b022b48417e1dc971a2673
Elif Tuba Korkmaz <etubasavli@gmail.com>
2023-07-18 10:56:30 +0300

    .

--- 01.Degıskenler/Program.cs
+++ 01.Degıskenler/Program.cs
@@ -17,3 +17,5 @@ char karakter = 'A';
 
 Console.WriteLine(nameLastName);
 
+
+Console.ReadLine();
---
+++ 04.SartBlogları/Program.cs
@@ -0,0 +1,64 @@
+﻿//if blogu
+//else if blogu
+//switch blogu
+
+int a = 1;
+int b = 2;
+
+//&& ve operatörü
+//|| veya operatörü
+// syntax bir kodun nasıl yazılması gerektğidir.
+
+//if (a < 3 && b == 1 || 3 == 3 || a <= 2 || a>= 1 || a != 1)
+//{ //scope
+//    Console.WriteLine(" a değeri 3 ten küçük bir değer");
+//}
+//else
+//{
+//    Console.WriteLine(" a değeri 3 veya 3 ten büyük bir değer");
+//}
+
+if (a > 0)
+{
+    if (a == 1)
+    {
+        //şunu yap
+    }
+
+    if (a == 2)
+    {
+        //şunu yap
+    }
+} else if (b == 1)
+{
+
+}
+else
+{
+
+}
+
+string name = "Elif";
+
+switch (name)
+{
+    case "Elif":
+        //işlem
+        break;
+    case "Halil":
+        //işlem
+        break;
+    default: 
+        //işlem
+        break;
+}
+
+switch (a)
+{
+    case < 3:
+        //işlem
+        break;
+    default:
+        //işlem
+        break;
+}
