d95b374889fc2a8b9ecd738c826694687e891289
Elif Tuba Korkmaz <etubasavli@gmail.com>
2023-07-18 11:12:26 +0300

    .

---
+++ 05.Listeler/Program.cs
@@ -0,0 +1,20 @@
+﻿// [] listeleri temsil eder array işareti
+
+//string[] names = { "Elif", "Tuba", "Korkmaz" };
+
+//string[] names = new string[3];
+//names[0] = "Elif";
+//names[1] = "Tuba";
+//names[2] = "Korkmaz";
+
+//List<string> names = new List<string>() { "Elif", "Tuba", "Korkmaz" };
+
+List<string> names = new(); //örneğe dönüştürmek için new kelimesini kullanıyoruz. Örneğe dönüştürme işlemine instance üretme deniyoruz.
+
+names.Add("Elif");
+names.Add("Elif");
+names.Add("Elif");
+names.Add("Elif");
+names.Add("Elif");
+names.Add("Elif");
+names.Add("Elif");
