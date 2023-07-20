d18562c9cd27b6467f60bbeac6013e04ddbce3db
Elif Tuba Korkmaz <etubasavli@gmail.com>
2023-07-20 13:23:46 +0300

    .

---
+++ 09.HataYonetimi/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+try
+{
+    int a = 0;
+    int b = 0;
+    int sonuc = a / b;
+} 
+catch (Exception ex)
+{
+    Console.WriteLine($"Hata : {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Ben halükarda çalışırım.");
+}
